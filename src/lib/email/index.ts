effe221d0182eb538a682747e1e2b5aaea6267a3
Blake Lange <blakelange@gmail.com>
2025-07-03 08:34:45 -0500

    Add pdf-lib, plaid dependencies and create email module index

---
+++ src/lib/email/index.ts
@@ -0,0 +1,6 @@
+// Email module exports
+export { sendEmail, EmailService } from './email-service';
+export { sendCollectionNotification } from './collection-notifications';
+export { sendConsultationNotification } from './consultation-notifications';
+export { sendInvoice } from './invoice-mailer';
+export { sendPaymentNotification } from './payment-notifications';
